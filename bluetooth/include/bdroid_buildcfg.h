839dd3640abebd6a4d7cf33f26230dac00be23b4
Art_Chen <Chenxy0201@qq.com>
2021-05-16 01:54:06 +0800

    bluetooth: Switch to QTI BT Stack again

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -29,13 +29,11 @@
 #define BTIF_HF_WBS_PREFERRED TRUE
 
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
-// Disables read remote device feature
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
+#define MAX_L2CAP_CHANNELS    32
 
 #define BLE_VND_INCLUDED   TRUE
+#define GATT_MAX_PHY_CHANNEL  10
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 #endif
