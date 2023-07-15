276db7e7ae6f44f4c76cb46d73f15e332af2fb12
Art_Chen <Chenxy0201@qq.com>
2023-07-15 13:44:41 +0800

    OP4A89: build drm 1.4 lazy since 1.3 is not the valid target
    Signed-off-by: Art_Chen <Chenxy0201@qq.com>

--- device.mk
+++ device.mk
@@ -268,11 +268,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service-lazy.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
